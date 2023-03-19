3b47431cb4401f29171c9ccfa62b145d8261773f
jasonkayzk <271226192@qq.com>
2023-03-19 09:24:20 +0800

    finished: chapter2

---
+++ async-framework/netty/chapter2/src/main/java/io/github/jasonkayzk/Configure.java
@@ -0,0 +1,8 @@
+package io.github.jasonkayzk;
+
+public class Configure {
+
+    public static final Integer PORT = 8848;
+
+    public static final String Endpoint = "localhost";
+}
---
+++ async-framework/netty/chapter2/src/main/java/io/github/jasonkayzk/server/EchoServer.java
@@ -0,0 +1,53 @@
+package io.github.jasonkayzk.server;
+
+import io.github.jasonkayzk.Configure;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+import java.net.InetSocketAddress;
+
+/**
+ * Listing 2.2 EchoServer class
+ */
+public class EchoServer {
+
+    private final Integer port;
+
+
+    public EchoServer(Integer port) {
+        this.port = port;
+    }
+
+    public void start() throws Exception {
+        final EchoServerHandler serverHandler = new EchoServerHandler();
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(group)
+                    .channel(NioServerSocketChannel.class)
+                    .localAddress(new InetSocketAddress(port))
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        public void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(serverHandler);
+                        }
+                    });
+
+            ChannelFuture f = b.bind().sync();
+            System.out.println(EchoServer.class.getName() +
+                    " started and listening for connections on " + f.channel().localAddress());
+            f.channel().closeFuture().sync();
+        } finally {
+            group.shutdownGracefully().sync();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        new EchoServer(Configure.PORT).start();
+    }
+}
